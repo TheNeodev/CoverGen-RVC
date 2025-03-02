5a03ede490561c9ca1d6be9c27cf2bf18e73ebea
NeoDev <neoforevershog@gmail.com>
2025-03-02 12:32:08 +0700

    Update CoverGen.py

--- src/CoverGen.py
+++ src/CoverGen.py
@@ -262,7 +262,7 @@ if __name__ == '__main__':
                                           chorus_mix, clipping_threshold, f0autotune, f0_min, f0_max,
                                           ai_cover, ai_vocals, main_vocals_dereverb, backup_vocals, instrumentals])
 
-    Andik, go fuck yourself =)
+    # Andik, go fuck yourself =)
 
     with gr.Tab('Model Upload'):
             with gr.Tab('Upload via Link'):
