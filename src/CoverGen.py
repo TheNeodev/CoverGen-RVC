14c72f0c99ea945b60bb1661b0a55782e6719783
NeoDev <neoforevershog@gmail.com>
2025-03-02 12:35:34 +0700

    Update CoverGen.py

--- src/CoverGen.py
+++ src/CoverGen.py
@@ -14,7 +14,10 @@ from main import song_cover_pipeline
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 mdxnet_models_dir = os.path.join(BASE_DIR, 'mdxnet_models')
+extraction_folder = os.path.join(BASE_DIR, 'rvc_models')
+
 rvc_models_dir = os.path.join(BASE_DIR, 'rvc_models')
+
 output_dir = os.path.join(BASE_DIR, 'song_output')
 
 def get_current_models(models_dir):
